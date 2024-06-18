e9ccb81bb4a81a0db2cf45fcd1d692db054dc6a1
Sivaranjani S <84496052+sivaranjanisg29@users.noreply.github.com>
2024-06-18 19:00:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteChar() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendChar(char) {
+    document.getElementById('display').value += char;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
